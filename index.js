d5c27d630bf8a322e48ba96dd235c096d29654fa
Jonathan Schapiro <jschapiro@MBPJSCHAPIRO.local>
2019-01-23 17:20:05 +0000

    utils/req.body

--- index.js
+++ index.js
@@ -1,28 +1,60 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongo = require('./db/submission');
+const isValidSubmission = require('./utils/validation/valid-submission');
 const app = express();
 const port = 3000;
 
-app.get('/submissions', (req, res) => res.send('Hello World!'));
-app.get('/submissions/:id', (req, res) => res.send('Hello World!'));
-
-app.post('/submissions', (req, res) => {
-    // create id;
-    /*
-        req.body = 
-        {
-            boa: true/false,
-            type: 'sub/sweep/pass',
-            position: 'closed guard/open guard',
-            positionName: 'arm lock',
-            yourBelt: 'white/blue/purple/brown/black,
-            theirBelt: 'white/blue/purple/brown/black,
-            date: 'utf date'
+let startUpTries = 2;
+const startUp = () => {
+    mongo.connect((err, db) => {
+        if (err) {
+            startUpTries = startUpTries - 1;
+            return startUpTries > 0 ? startUp() : process.exit();
         }
-    */
-});
 
-app.delete('/submissions/:id', (req, res) => {
+        app.use(bodyParser.urlencoded({ extended: false }));
 
-});
+        app.get('/submissions', (req, res) => res.send('Hello World!'));
+        app.get('/submissions/:id', (req, res) => res.send('Hello World!'));
+        
+        app.post('/submissions', (req, res) => {
+            const submission = req.body;
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+            if (!submission || !isValidSubmission(submission)) {
+                return res.send(404);
+            }
+
+            return mongo.insert(db, submission, (err, result) => {
+                if (err || !result) {
+                    return res.send(404);
+                }
+
+                return res.send(200);
+            });
+            // create id;
+            /*
+                req.body = 
+                {
+                    _id: '123', // on the object already
+                    user: 'jschapir',
+                    boa: true/false,
+                    type: 'sub/sweep/pass',
+                    position: 'closed guard/open guard',
+                    positionName: 'arm lock',
+                    yourBelt: 'white/blue/purple/brown/black,
+                    theirBelt: 'white/blue/purple/brown/black,
+                    date: new Date()
+                }
+            */
+        });
+        
+        app.delete('/submissions/:id', (req, res) => {
+        
+        });
+        
+        app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+    });
+};
+
+startUp();
---
+++ utils/validation/valid-submission.js
@@ -0,0 +1,3 @@
+export default (submission) => {
+    return true;
+}
